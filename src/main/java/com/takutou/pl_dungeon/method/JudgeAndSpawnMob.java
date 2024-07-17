8f0d7e0299abcb17cdd5ddd5778edb4ecf0e2fc8
TakutoMatsui <takuto1488@gmail.com>
2024-07-17 17:21:52 +0900

    通常のスポーンエッグの機能は無効化する

--- src/main/java/com/takutou/pl_dungeon/method/JudgeAndSpawnMob.java
+++ src/main/java/com/takutou/pl_dungeon/method/JudgeAndSpawnMob.java
@@ -1,9 +1,13 @@
 package com.takutou.pl_dungeon.method;
 
+import com.takutou.pl_dungeon.mob.DungeonZombie;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
-
+/*
+* judgeAndSpawn: ここで指定したIDを持ったアイテムか判定する
+* */
 public class JudgeAndSpawnMob {
     String entityName ;
     PlayerInteractEvent e;
@@ -17,7 +21,19 @@ public class JudgeAndSpawnMob {
         if(e.getItem() != null && e.getItem().getItemMeta() != null
             && e.getItem().getItemMeta().getLore() !=null
             && e.getItem().getItemMeta().getLore().contains(ChatColor.BLUE + "Spawns a dungeon:" + entityName)){
+            //標準のスポーンエッグの機能を無効化
+            e.setCancelled(true);
+            //右クリックしたアイテムのメタデータ
             player.sendMessage(ChatColor.BLUE + "Spawns a dungeon:" + entityName);
+            //右クリック先のブロックにスポーン位置を設定
+            Location spawnLocation;
+            if(e.getClickedBlock().isPassable()){
+                spawnLocation = e.getClickedBlock().getLocation().add(0.5,0,0.5);
+            }else{
+                spawnLocation = e.getClickedBlock().getRelative(e.getBlockFace()).getLocation().add(0.5,0,0.5);
+            }
+            //モンスターをスポーン
+            new DungeonZombie(spawnLocation);
         }
     }
 }
