bd0c92922488b72d33ad1696d84eafbd9d1e697b
tacotaco0808 <takuto1488@gmail.com>
2024-09-04 23:41:41 +0900

    不要なファイル削除

--- src/main/java/com/takutou/pl_dungeon/Pl_dungeon.java
+++ src/main/java/com/takutou/pl_dungeon/Pl_dungeon.java
@@ -2,12 +2,9 @@ package com.takutou.pl_dungeon;
 
 import com.takutou.pl_dungeon.command.CreateDungeonMob;
 import com.takutou.pl_dungeon.command.DungeonEntitySpawnEgg;
-import com.takutou.pl_dungeon.command.DungeonZombieSpawnEgg;
 import com.takutou.pl_dungeon.listener.EntityDeathListener;
 import com.takutou.pl_dungeon.listener.InteractListener;
 import com.takutou.pl_dungeon.method.MobManager;
-import com.takutou.pl_dungeon.mob.TestZombie;
-import org.bukkit.entity.Mob;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Pl_dungeon extends JavaPlugin {
@@ -16,14 +13,10 @@ public final class Pl_dungeon extends JavaPlugin {
     public void onEnable() {
         mobManager = new MobManager(this);
         // Plugin startup logic
-        SpawnEntity spawnEntity = new SpawnEntity(this);
         // コマンドエクゼキューターの登録
-        this.getCommand("spawnzombie").setExecutor(spawnEntity);
-        this.getCommand("givedungeonzombieegg").setExecutor(new DungeonZombieSpawnEgg());
         this.getCommand("dungeon").setExecutor(new DungeonEntitySpawnEgg(this));
         this.getCommand("dungeoncreate").setExecutor(new CreateDungeonMob(mobManager));
         // イベントリスナーの登録
-        getServer().getPluginManager().registerEvents(spawnEntity, this);
         getServer().getPluginManager().registerEvents(new InteractListener(mobManager,this),this);
         getServer().getPluginManager().registerEvents(new EntityDeathListener(mobManager,this),this);
 
