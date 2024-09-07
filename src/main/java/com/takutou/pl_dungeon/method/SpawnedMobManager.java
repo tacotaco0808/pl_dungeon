5942a59ec704542ea413c7c065356f79c07f9a64
tacotaco0808 <takuto1488@gmail.com>
2024-09-08 00:37:35 +0900

    座標とmobKeyのデータをspawnedMobs.ymlに保存

--- src/main/java/com/takutou/pl_dungeon/method/SpawnedMobManager.java
+++ src/main/java/com/takutou/pl_dungeon/method/SpawnedMobManager.java
@@ -1,10 +1,13 @@
 package com.takutou.pl_dungeon.method;
 
 import com.takutou.pl_dungeon.mob.EntityObject;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +19,28 @@ public class SpawnedMobManager {
     private FileConfiguration mobDataConfig;
     public SpawnedMobManager(Plugin plugin){
         this.plugin = plugin;
+        this.mobDataFile = new File(plugin.getDataFolder(),"spawnedmobs.yml");
+        this.mobDataConfig = YamlConfiguration.loadConfiguration(mobDataFile);
     }
     public void pushSpawnedDungeonMobs(EntityObject mob){
         spawnedDungeonMobs.add(mob);
-
+        this.saveMobData(mob);
     }
+    private void saveMobData(EntityObject mob) {
+        String path = "mobs." + mob.getMonsterID().toString();
+        Location location = mob.getMonsterSpawnLocation();
+        mobDataConfig.set(path + ".mobKey", mob.getMobKey());
+        mobDataConfig.set(path + ".location.world", location.getWorld().getName());
+        mobDataConfig.set(path + ".location.x", location.getX());
+        mobDataConfig.set(path + ".location.y", location.getY());
+        mobDataConfig.set(path + ".location.z", location.getZ());
+        try {
+            mobDataConfig.save(mobDataFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /*getter*/
     public List<EntityObject> getAllDungeonMobs() {
         return spawnedDungeonMobs;
